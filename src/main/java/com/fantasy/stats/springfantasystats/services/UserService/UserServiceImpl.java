0ff99bcf524df776b10056a8e320cd808102fb99
Edi Sincek <edi.sincek@gmail.com>
2024-05-06 20:10:31 +0200

    Adding post endpoints

--- src/main/java/com/fantasy/stats/springfantasystats/services/UserService/UserServiceImpl.java
+++ src/main/java/com/fantasy/stats/springfantasystats/services/UserService/UserServiceImpl.java
@@ -20,4 +20,8 @@ public class UserServiceImpl implements UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 }
